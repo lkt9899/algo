f9f7364e3a9a839ec414385dbd1be0b794fe08b2
lkt9899 <lkt9899@naver.com>
2024-01-14 22:04:27 +0900

    change directory

--- eunbi/algorithmStudy/src/com/ssafy/algoStudy/BJ/Main_2022_사다리.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.algoStudy.BJ;
-
-public class Main_2022_사다리 {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- eunbi/algorithmStudy/src/com/ssafy/algoStudy/Programmers/Solution_입국심사.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.algoStudy.Programmers;
-
-public class Solution_입국심사 {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
